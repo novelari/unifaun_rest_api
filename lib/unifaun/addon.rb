17870f1df5c097a2e6e1d73904cd03c3ed25cbf6
elnaqah <elnaqah@novelari.com>
2017-03-02 13:24:19 +0200

    add service addon

---
+++ lib/unifaun/addon.rb
@@ -0,0 +1,6 @@
+class AddOn<Base
+  attr_accessor :id
+  def initialize(id)
+    @id = id
+  end
+end
--- lib/unifaun/service.rb
+++ lib/unifaun/service.rb
@@ -1,6 +1,11 @@
+require "unifaun/addon.rb"
 class Service<Base
   attr_accessor :id
-  def initialize(id)
+  attr_accessor :paymentMethodType
+  attr_accessor :addons
+  def initialize(id,paymentMethodType="*NONE*",addons)
     @id = id
+    @addons = addons.map{|s| AddOn.new(s)}
+    @paymentMethodType = paymentMethodType
   end
 end
